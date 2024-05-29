873d52c1c692c63c45b9491716709b2991b7b251
TCBOM <TheCompleteBookOfMormon@gmail.com>
2024-05-29 16:13:13 +0100

    Delete angular

--- Source/AddMigration.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-dotnet ef migrations add "%1" --project "TheCompleteBookOfMormon.Web.Server\TheCompleteBookOfMormon.Web.Server.csproj" --startup-project "TheCompleteBookOfMormon.Web.Server\TheCompleteBookOfMormon.Web.Server.csproj" --output-dir "Migrations" -c ApplicationDbContext
-pause
--- Source/UpdateDB.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-dotnet ef database update --project "TheCompleteBookOfMormon.Web.Server\TheCompleteBookOfMormon.Web.Server.csproj" --startup-project "TheCompleteBookOfMormon.Web.Server\TheCompleteBookOfMormon.Web.Server.csproj" -c ApplicationDbContext
-pause
