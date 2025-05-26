b18ea7771f71347e41c83b105c45dcc005d50468
TCBOMProject <thecompletebookofmormon@gmail.com>
2025-05-26 19:45:20 +0100

    Delete words to left or right

--- Source/WordsAnalysis/WordsAnalysis/Components/DeleteWordsDialog.razor.cs
+++ Source/WordsAnalysis/WordsAnalysis/Components/DeleteWordsDialog.razor.cs
@@ -1,7 +1,10 @@
 using DocumentsModel;
+using DocumentsModel.Helpers;
+using ImageMagick;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 using WordsAnalysis.AppLayer.Features.SyncDocuments;
+using WordsAnalysis.Extensions;
 
 namespace WordsAnalysis.Components;
 
@@ -17,10 +20,26 @@ public partial class DeleteWordsDialog
     public FluentDialog Dialog { get; set; } = default!;
 
     private int DeleteCount;
+    private int DeletePosition;
+    private OcrElement FirstElement = null!;
+    private WordReference FirstWordReference = null!;
+    private string PageImageData = "";
+    private int PageHeight;
+    private int PageWidth;
+    private DeleteWordsPosition Position = DeleteWordsPosition.Sequential;
 
     protected override void OnInitialized()
     {
         base.OnInitialized();
+        string imageFilePath = FilePathHelper.GetScansDeskewedImageFilePath(AppLayer.Constants.Data.SourcesDirectoryPath, Content.EditionState.BookInfo, Content.Words.First().Key.PageNumber);
+        using var pageImage = new MagickImage(imageFilePath);
+        PageHeight = (int)pageImage.Height;
+        PageWidth = (int)pageImage.Width;
+        PageImageData = pageImage.ToEmbeddedHtmlImage()!;
+
+        FirstWordReference = Content.Words.First().Key;
+        var firstWord = Content.EditionState.LoadedPages[FirstWordReference.PageNumber].Page.Words[FirstWordReference.WordIndex]!;
+        FirstElement = firstWord.Elements.Last();
 
         // About index
         int? aboutIndex = Content.Words.Select((word, index) => new { Index = index, Text = word.Value }).FirstOrDefault(x => string.Equals("about", x.Text, StringComparison.InvariantCultureIgnoreCase) && x.Text![0] == 'A')?.Index;
@@ -35,8 +54,6 @@ public partial class DeleteWordsDialog
 
         // Next column index
         int? nextColumnIndex = null;
-        OcrWord firstWord = Content.Words[0].Key.GetWord(Content.EditionState)!;
-        OcrElement firstElement = firstWord.Elements.Last();
         // Try to find start of next column
         for (int index = 2; index < Content.Words.Length; index++)
         {
@@ -44,7 +61,7 @@ public partial class DeleteWordsDialog
             if (candidateWord is null) continue;
 
             OcrElement candidateElement = candidateWord.Elements.Last();
-            if (candidateElement.Bounds.Y + candidateElement.Bounds.Height < firstElement.Bounds.Y)
+            if (candidateElement.Bounds.Y + candidateElement.Bounds.Height < FirstElement.Bounds.Y)
             {
                 nextColumnIndex = index;
                 break;
@@ -71,12 +88,43 @@ public partial class DeleteWordsDialog
 
     private async Task ConfirmAsync()
     {
-        var result = new DeleteWordsDialogResult(Content.Words.Take(DeleteCount).Select(x => x.Key).ToArray());
+        int pageNumber = FirstWordReference.PageNumber;
+        var page = Content.EditionState.LoadedPages[pageNumber].Page;
+        var wordReferences = Content.Words
+            .Select(x => x.Key)
+            .Where(x => x != null && x.PageNumber == pageNumber)
+            .Select(x => new { WordReference = x, Element = page.Words[x.WordIndex]!.Elements[0] });
+
+        WordReference[] wordsToDelete = Position switch {
+            DeleteWordsPosition.Left => wordReferences.Where(x => x.Element.Bounds.X <= DeletePosition).Select(x => x.WordReference).ToArray(),
+            DeleteWordsPosition.Right => wordReferences.Where(x => x.Element.Bounds.GetRight() >= DeletePosition).Select(x => x.WordReference).ToArray(),
+            _ => Content.Words.Take(DeleteCount).Select(x => x.Key).ToArray(),
+        };
+
+        var result = new DeleteWordsDialogResult(wordsToDelete);
         await Dialog.CloseAsync(result);
     }
+
+    private void PositionTypeChanged()
+    {
+        if (FirstElement == null) return;
+        DeletePosition = Position switch {
+            DeleteWordsPosition.Left => FirstElement.Bounds.GetRight(),
+            DeleteWordsPosition.Right => FirstElement.Bounds.X,
+            _ => DeletePosition
+        };
+    }
+        
 
     private void SetDeleteCount(int count)
     {
         DeleteCount = count;
+    }
+
+    private enum DeleteWordsPosition
+    {
+        Sequential,
+        Left,
+        Right
     }
 }
