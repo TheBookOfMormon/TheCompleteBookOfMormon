a88d43516b98b70d26d2b546ebcd75b76d05b747
TCBOMProject <thecompletebookofmormon@gmail.com>
2025-07-13 12:28:03 +0100

    Elements not required

--- Source/DocumentsModel/OcrWord.cs
+++ Source/DocumentsModel/OcrWord.cs
@@ -7,7 +7,7 @@ namespace DocumentsModel;
 [DebuggerDisplay("{GetDisplayText()}")]
 public record OcrWord
 {
-    public required ImmutableList<OcrElement> Elements { get; init; }
+    public ImmutableList<OcrElement> Elements { get; init; } = [];
 
     public string GetCombinedText() => string.Join("", Elements.Where((x, index) => ShowDashes || index == 0 || x.Text != "-").Select(x => x.Text));
     public string GetDisplayText(bool showBenefitOfDoubt) =>
