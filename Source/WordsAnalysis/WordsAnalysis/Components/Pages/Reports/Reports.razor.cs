c44b2cda0edd293a97b38831ce4964c5459e3839
TCBOMProject <thecompletebookofmormon@gmail.com>
2025-07-19 11:32:22 +0100

    Use Page + WordIndex + Word for data

--- Source/WordsAnalysis/WordsAnalysis/Components/Pages/Reports/Reports.razor.cs
+++ Source/WordsAnalysis/WordsAnalysis/Components/Pages/Reports/Reports.razor.cs
@@ -6,10 +6,17 @@ public partial class Reports : IDisposable
 {
     private State CurrentState;
     private EditionProcessor EditionProcessor = null!;
-    private Dictionary<OcrBookInfo, Dictionary<int, OcrPage>>? Editions;
+    private Dictionary<OcrBookInfo, WordEntry[]>? Editions;
     private EditionHierarchyData HierarchyData = null!;
     private readonly Loader Loader;
     private Dictionary<OcrBookInfo, Dictionary<OcrBookInfo, decimal>> SimilarityTableData = null!;
+
+    private readonly struct WordEntry
+    {
+        public required int PageNumber { get; init; }
+        public required int WordIndex { get; init; }
+        public required OcrWord? Word { get; init; }
+    }
 
     private enum State
     {
@@ -45,7 +52,8 @@ public partial class Reports : IDisposable
         StateHasChanged();
         await Task.Yield();
 
-        Editions = Loader.GetEditions();
+        Dictionary<OcrBookInfo, Dictionary<int, OcrPage>> editions = Loader.GetEditions();
+        Editions = ConvertEditionsData(editions);
         await DetermineHierarchyAsync();
 
         CurrentState = State.Finished;
@@ -59,10 +67,36 @@ public partial class Reports : IDisposable
         Dictionary<OcrBookInfo, IEnumerable<OcrWord?>> editionsWords = Editions!
              .ToDictionary(
                  x => x.Key,
-                 x => x.Value.OrderBy(x => x.Key).SelectMany(x => x.Value.Words));
+                 x => x.Value.Select(x => x.Word));
 
         SimilarityTableData = EditionSimilarityTableBuilder.Build(editionsWords);
         HierarchyData = EditionHierarchyDataBuilder.Build(SimilarityTableData);
         return Task.CompletedTask;
     }
+
+    private static Dictionary<OcrBookInfo, WordEntry[]>? ConvertEditionsData(Dictionary<OcrBookInfo, Dictionary<int, OcrPage>> editions)
+    {
+        var result = new Dictionary<OcrBookInfo, WordEntry[]>();
+        foreach (var editionKvp in editions)
+        {
+            var words = new List<WordEntry>(300000);
+            foreach(var pageKvp in editionKvp.Value.OrderBy(x => x.Key))
+            {
+                int wordIndex = 0;
+                foreach(var word in pageKvp.Value.Words)
+                {
+                    var entry = new WordEntry {
+                        PageNumber = pageKvp.Key,
+                        WordIndex = wordIndex++,
+                        Word = word
+                    };
+                    words.Add(entry);
+                }
+            }
+            result.Add(editionKvp.Key, words.ToArray());
+        }
+        return result;
+    }
+
+
 }
