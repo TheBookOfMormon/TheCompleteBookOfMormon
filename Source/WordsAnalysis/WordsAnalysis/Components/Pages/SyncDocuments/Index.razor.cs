86846348ad19ff78bf971a3fa241a3a9a470bc11
TCBOMProject <thecompletebookofmormon@gmail.com>
2025-09-01 17:49:42 +0100

    Show strikethrough even if word is correction

--- Source/WordsAnalysis/WordsAnalysis/Components/Pages/SyncDocuments/Index.razor.cs
+++ Source/WordsAnalysis/WordsAnalysis/Components/Pages/SyncDocuments/Index.razor.cs
@@ -63,7 +63,7 @@ public partial class Index : IDisposable
     }
 
     private string? GetWordStyle(OcrWord? word) =>
-        (!ViewModel.ShowBenefitOfDoubt && word?.Corrected == true && word?.Correction != true) ? "text-decoration: line-through" : null;
+        (!ViewModel.ShowBenefitOfDoubt && word?.Corrected == true) ? "text-decoration: line-through" : null;
 
     private string GetZeroPaddedSectionNumber(int sectionNumber)
     {
